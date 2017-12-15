7d68e33bd74c3db77f0fe7aa44aae9001ca239b9
Rosina Norton <rn9700@gmail.com>
2017-12-14 19:35:37 -0800

    including title in word vect

--- model_recommendation.py
+++ model_recommendation.py
@@ -11,7 +11,7 @@ from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
 
 def get_feature_data():
     df = pd.read_csv('data/all_with_features.csv')
-    cols_to_keep = [u'title',u'Unnamed: 0', u'description', u'number_of_positions', u'required_work_experience_months',
+    cols_to_keep = [u'title', u'orig_idx', u'description', u'number_of_positions', u'required_work_experience_months',
        u'campaign_start_date_year', u'campaign_start_date_month', u'campaign_start_date_day',
        u'campaign_start_date_dow', u'campaign_duration', u'desc_length',
        u'country_code_CI', u'country_code_DZ', u'country_code_KE',
@@ -45,66 +45,81 @@ def get_original_data():
     df['campaign_end_date'] = pd.to_datetime(df['campaign_end_date'])
     return df
 
-def generate_recommendation(n, train, df_orig, top_5):
-    idx = list(train.index.values).index(n)
-    print 'train index {}'.format(idx)
-    # idx_select = train.index.values == n
-    original_index = train.iloc[idx,:]['Unnamed: 0']
-    print original_index
+def vectorise_text(data, column):
+    pass
+
+def generate_recommendations(idx, train, df_orig):
+    original_index = train.iloc[idx,:]['orig_idx']
+    print 'original_index: {}'.format(original_index)
     print train.iloc[idx,:]['title']
     start_date = df_orig.iloc[original_index,:]['campaign_start_date']
     end_date = df_orig.iloc[original_index,:]['campaign_end_date']
     print df_orig.iloc[original_index,:][['title','job_level','country','campaign_start_date','campaign_end_date','description','employer_name']]
-    # print train.iloc[n,:]['title']
     print '\nTop similar jobs .......'
-
-    top_5_orig_index = train.iloc[top_5[idx],:]['Unnamed: 0'].values
-    print top_5[idx]
-    print top_5_orig_index
+    top_5_orig_index = train.iloc[idx,:]['top_recs']
+    print 'top original indexes: {}'.format(top_5_orig_index)
+    print 'distances: {}'.format(train.iloc[idx,:]['top_dists'])
     temp = df_orig.iloc[top_5_orig_index,:]
     print temp[temp['campaign_end_date']>start_date][['title','job_level','country','campaign_start_date','campaign_end_date','description','employer_name']]
-    # print train.iloc[top_5[n],:]['title']
     print '----------------------------------------------\n'
     pass
+
+def get_vocab(vocab_list, included):
+    mask = included > 0
+    vocab_list = np.asarray(vocab_list)
+    vocab = vocab_list[mask]
+    return vocab
 
 if __name__=='__main__':
     df = get_feature_data()
     df_orig = get_original_data()
     punctuation = set(string.punctuation)
     stemmer = SnowballStemmer('english')
+
     df['desc_no_punc'] = df.description.map(lambda x: ''.join(ch for ch in x if x not in punctuation))
-    df['desc_stemmed'] = df.description.map(lambda x: ' '.join([stemmer.stem(y) for y in x.decode('utf-8').split(' ')]))
+    df['desc_stemmed'] = df.description.map(lambda x: ' '.join([stemmer.stem(y) for y in x.decode('utf-8').split(' ') if y.isalpha()]))
+
+    df['title_no_punc'] = df.title.map(lambda x: ''.join(ch for ch in x if x not in punctuation))
+    df['title_stemmed'] = df.title.map(lambda x: ' '.join([stemmer.stem(y) for y in x.decode('utf-8').split(' ') if y.isalpha()]))
 
     train = df
-    # train, test = train_test_split(df,test_size=0.01, random_state=42)
+    train = train.reset_index()
 
-    tf_vec = TfidfVectorizer(max_df=0.6, min_df=0.001, stop_words='english',ngram_range=(1,1),max_features=50)
-    tf_vec = tf_vec.fit(train['desc_stemmed'].values)
-    train_vector = tf_vec.transform(train['desc_stemmed'].values).toarray()
-    # test_vector = tf_vec.transform(test['desc_stemmed'].values).toarray()
+    tf_vec_desc = TfidfVectorizer(max_df=0.8, min_df=0.001, stop_words='english',ngram_range=(1,3),max_features=80)
+    tf_vec_desc = tf_vec_desc.fit(train['desc_stemmed'].values)
+    train_vector_desc = tf_vec_desc.transform(train['desc_stemmed'].values).toarray()
 
-    train2 = train.drop(['description','desc_no_punc','desc_stemmed','Unnamed: 0','title'],axis=1)
-    # test2 = test.drop(['description','desc_no_punc','desc_stemmed','Unnamed: 0'],axis=1)
+    tf_vec_title = TfidfVectorizer(max_df=0.5, min_df=0.001, stop_words='english',ngram_range=(1,3),max_features=35)
+    tf_vec_title = tf_vec_title.fit(train['title_stemmed'].values)
+    train_vector_title = tf_vec_title.transform(train['title_stemmed'].values).toarray()
 
-    train2 = np.concatenate((train2.values,train_vector),axis=1)
-    # test2 = np.concatenate((test2.values, test_vector),axis=1)
+
+    train2 = train.drop(['description','desc_no_punc','desc_stemmed','title_no_punc','title_stemmed','orig_idx','title'],axis=1)
+    train2 = np.concatenate((train2.values,train_vector_desc),axis=1)
+    train2 = np.concatenate((train2,train_vector_title),axis=1)
+
 
     scaler = StandardScaler()
     scaler = scaler.fit(train2)
     train_scaled = scaler.transform(train2)
-    # test_scaled = scaler.transform(test2)
 
-    pca = PCA(n_components=20)
+    pca = PCA(n_components=30)
     pca = pca.fit(train_scaled)
     train_reduced = pca.transform(train_scaled)
     # test_reduced = pca.transform(test_scaled)
 
     distances = cosine_similarity(train_reduced).T
     # distances = linear_kernel(train_reduced).T
-    top_5 = []
+    top_5_orig = []
+    top_distances = []
     for i in xrange(len(train_reduced)):
         top = distances[i,:].argsort()[-6:-1][::-1]
-        top_5.append(top)
+        dists  = distances[i,top]
+        top_orig = train.iloc[top,:]['orig_idx'].values
+        top_5_orig.append(top_orig)
+        top_distances.append(dists)
 
+    train['top_recs'] = pd.Series(top_5_orig)
+    train['top_dists'] = pd.Series(top_distances)
 
-    generate_recommendation(100, train, df_orig, top_5)
+    # generate_recommendation(100, train, df_orig, distances)
